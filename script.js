0ab9ccbcf5027aeda3e3d425a2b21ccfa8151689
Gyanvi <gyanvi0109@gmail.com>
2022-06-11 19:06:59 +0530

    initial commit after modifying code in javascript

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var audio = document.createElement('audio');
-audio.setAttribute('src', 'sound.mp3');
+audio.setAttribute('src','sound.mp3');
 audio.loop=true;
+audio.play();
+audio.muted=false;
 audio.play();
